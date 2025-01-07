39831b1c59591eb078c036a991b4ad8e423f6fe8
Zaidan Ahmad <1fact28@gmail.com>
2025-01-07 10:27:12 +0700

    Fixed
    Signed-off-by: Zaidan Ahmad <1fact28@gmail.com>

--- src/main/java/keltiga/MechiuApp.java
+++ src/main/java/keltiga/MechiuApp.java
@@ -3,18 +3,39 @@ package keltiga;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import keltiga.controller.SceneManager;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.PrintStream;
+import java.io.FileOutputStream;
+import java.io.File;
 
 public class MechiuApp extends Application {
+    private static PrintWriter logFile;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        SceneManager.setStage(primaryStage);
-        SceneManager.switchToUserSelection();
-        primaryStage.setTitle("Typer Game - Mechiu");
-        primaryStage.show();
+        try {
+            // Setup logging
+            logFile = new PrintWriter(new FileWriter("mechiu-debug.log", true));
+            System.setOut(new PrintStream(new FileOutputStream("mechiu-debug.log", true)));
+
+            SceneManager.initialize(primaryStage);
+            SceneManager.switchToUserSelection();
+            primaryStage.setTitle("Typer Game - Mechiu");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void stop() {
+        if (logFile != null) {
+            logFile.close();
+        }
     }
 }
